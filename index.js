058f9b2e4f754fc854f81296bb861d29f0eda3ce
cacau <anaclaudia.sza@outlook.com>
2024-07-28 16:41:58 -0300

    initial commit

--- index.js
+++ index.js
@@ -4,3 +4,4 @@ import NeomorphBlur from './src/NeomorphBlur';
 import ShadowFlex from './src/ShadowFlex';
 import NeomorphFlex from './src/NeomorphFlex';
 export { Shadow, Neomorph, NeomorphBlur, ShadowFlex, NeomorphFlex };
+// first commit
